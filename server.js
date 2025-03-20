1ed115c5d92ba4fd442951917d19de3c4ce83f57
Yossab_Samuel <yossabpro5@gmail.com>
2025-03-20 07:59:35 +0200

    small changes in server.js and package .json

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const http = require("http");
 
 const server = http.createServer((request, response) => {
 
-})
-
+  if (request.url == "/api/products" && request.method == "GET") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(products));
+  } else {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ error: "404 Not Found :(" }));
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`server is on port ${PORT}`);
-})
+});
